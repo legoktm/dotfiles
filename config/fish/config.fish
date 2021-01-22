83d916d6c2f428abfa2b240ba2ea1f663a75e5ad
Kunal Mehta <legoktm@member.fsf.org>
2021-01-21 17:28:54 -0800

    Sync

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,7 @@
 set -x EDITOR nano
 set -x PATH "$HOME/.cargo/bin:$PATH"
-set -x DEBEMAIL "legoktm@debian.org"
+set -x DEBEMAIL "legoktm@member.fsf.org"
 set -x DEBFULLNAME "Kunal Mehta"
 set -x ROCKET_CLI_COLORS false
+set -x PHAN_DISABLE_XDEBUG_WARN 1
+set -x JENKINS_USERNAME "legoktm"
