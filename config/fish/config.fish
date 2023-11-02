a0f2883e37d34b268041d9d1863a7e96bcb1bb0b
Kunal Mehta <legoktm@debian.org>
2023-11-02 01:20:52 -0400

    Updates

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,5 @@
 set -x EDITOR nano
-set -x PATH "$HOME/gogs/bin:$HOME/.cargo/bin:$PATH"
+set -x PATH "$HOME/gogs/bin:$HOME/.cargo/bin:$HOME/.local/bin:$PATH"
 set -x DEBEMAIL "legoktm@debian.org"
 set -x DEBFULLNAME "Kunal Mehta"
 set -x ROCKET_CLI_COLORS false
@@ -9,4 +9,4 @@ set -x QUBES_GPG_DOMAIN vault-gpg
 set -x USE_PODMAN 1
 set -x FPF_USERNAME legoktm
 set -x SOPS_USERNAME legoktm
-set -x SOPS_GPG_EXEC "qubes-gpg-client"
+set -x SOPS_GPG_EXEC "qubes-gpg-client-wrapper"
